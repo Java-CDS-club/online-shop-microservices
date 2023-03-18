620ebcdac5cee0941ce8fa8da62e450507a898de
Mohan Mohadikar <mohanmohadikar@Mohans-MacBook-Air.local>
2023-03-18 14:21:08 +0530

    added create product & get all product endpoints.

--- src/main/java/com/mohanmmohadikar/productservice/controller/ProductController.java
+++ src/main/java/com/mohanmmohadikar/productservice/controller/ProductController.java
@@ -1,4 +1,29 @@
 package com.mohanmmohadikar.productservice.controller;
 
+import com.mohanmmohadikar.productservice.dto.ProductRequest;
+import com.mohanmmohadikar.productservice.dto.ProductResponse;
+import com.mohanmmohadikar.productservice.service.ProductService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+  private final ProductService productService;
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public void createProduct(@RequestBody ProductRequest productRequest) {
+    productService.createProduct(productRequest);
+  }
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public List<ProductResponse> getAllProducts() {
+    return productService.getAllProducts();
+  }
 }
