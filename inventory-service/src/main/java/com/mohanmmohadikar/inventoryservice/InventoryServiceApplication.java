73e30918310e390d92e0129fb95a0361a63272cb
mohanmohadikar <mohanmmohadikar@gmail.com>
2023-03-23 18:01:05 +0530

    added eureka discovery server

--- inventory-service/src/main/java/com/mohanmmohadikar/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/mohanmmohadikar/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.mohanmmohadikar.inventoryservice;
 
-import com.mohanmmohadikar.inventoryservice.model.Inventory;import com.mohanmmohadikar.inventoryservice.repository.InventoryRepository;import org.springframework.boot.CommandLineRunner;import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.context.annotation.Bean;
+import com.mohanmmohadikar.inventoryservice.model.Inventory;
+import com.mohanmmohadikar.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
