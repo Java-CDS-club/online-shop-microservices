31ce64e7393c09765797fae32fff66f7197bae74
mohanmohadikar <mohanmmohadikar@gmail.com>
2023-03-23 15:57:42 +0530

    added web client in order service to communicate with inventory service

--- inventory-service/src/main/java/com/mohanmmohadikar/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/mohanmmohadikar/inventoryservice/repository/InventoryRepository.java
@@ -1,10 +1,9 @@
 package com.mohanmmohadikar.inventoryservice.repository;
 
 import com.mohanmmohadikar.inventoryservice.model.Inventory;
-import java.util.Optional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-  Optional<Inventory> findBySkuCode(String skuCode);
-}
+
+  List<Inventory>  findBySkuCodeIn(List<String> skuCode);}
--- inventory-service/src/main/java/com/mohanmmohadikar/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/mohanmmohadikar/inventoryservice/service/InventoryService.java
@@ -1,6 +1,6 @@
 package com.mohanmmohadikar.inventoryservice.service;
 
-import com.mohanmmohadikar.inventoryservice.repository.InventoryRepository;import lombok.RequiredArgsConstructor;
+import com.mohanmmohadikar.inventoryservice.dto.InventoryResponse;import com.mohanmmohadikar.inventoryservice.repository.InventoryRepository;import java.util.List;import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,7 +11,16 @@ public class InventoryService {
   private final InventoryRepository inventoryRepository;
 
   @Transactional(readOnly = true)
-  public boolean isInStock(String skuCode) {
-    return inventoryRepository.findBySkuCode(skuCode).isPresent();
+  public List<InventoryResponse> isInStock( List<String> skuCode) {
+    return inventoryRepository.findBySkuCodeIn(skuCode)
+        .stream()
+        .map(inventory ->
+              InventoryResponse.builder()
+                  .skuCode(inventory.getSkuCode())
+                  .isInStock(inventory.getQuantity()>0)
+                  .build()
+            )
+        .toList();
+
   }
 }
